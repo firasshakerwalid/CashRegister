d37b7b5397c1f6d1513f7c83aba46091cc411423
FirasMaya <firasshakerwalid@hotmail.com>
2018-08-30 14:13:55 -0700

    .

--- CashRegister/Program.cs
+++ CashRegister/Program.cs
@@ -1,57 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 namespace CashRegister
 {
  class Program
  {
-  static void Main(string[] args)
+  static void Main()
   {
-   Console.WriteLine("Welcome to XXX Store");
-   double itemTotalPrice = 0;
-   var allItems = new List<Item>();
    while (true)
    {
-    Console.Write("enter Item ID ('0' to continue): ");
-    Item currentItem = GetItemId();
-    
-    if (currentItem == null) { break; }
-    double itemPrice;
-    if (currentItem.IsWeight)
+    WriteLine("Welcome to XXX Store");
+    double itemTotalPrice = 0;
+    var allItems = new List<Item>();
+    while (true)
     {
-     Console.Write("weight:");
-     var itemWeight = float.Parse(Console.ReadLine());
-     itemPrice = itemWeight * currentItem.Price ;
-     itemTotalPrice += itemPrice;
-
+     Write("enter Item ID ('0' to continue): ");
+     Item currentItem = GetItemId();
+     if (currentItem == null) { break; }
+     double itemPrice;
+     if (currentItem.IsWeight)
+     {
+      Write("weight:");
+      float itemWeight = -1;
+      while (itemWeight == -1)
+      {
+       try { itemWeight = float.Parse(ReadLine()); }
+       catch (Exception) { WriteLine("Incorect value, try again"); }
+      }
+      itemPrice = itemWeight * currentItem.Price;
+      itemTotalPrice += itemPrice;
+     }
+     else
+     {
+      Write("quantity:");
+      var itemQuantity = -1;
+      while (itemQuantity == -1)
+      {
+       try { itemQuantity = int.Parse(ReadLine()); }
+       catch (Exception) { WriteLine("Incorect value, try again"); }
+      }
+      if (currentItem.QuantityCoupon != 0) { itemQuantity -= Math.Abs(itemQuantity / currentItem.QuantityCoupon); }
+      itemPrice = itemQuantity * currentItem.Price;
+      itemTotalPrice += itemPrice;
+     }
+     WriteLine("{0}:{1}$", currentItem.Name.TrimEnd(), itemPrice);
+     allItems.Add(new Item() {Name = currentItem.Name.TrimEnd(), Price = itemPrice});
     }
-    else
+    WriteLine("Enter %  discount coupons of total price");
+    var discountCoupon = -1;
+    while (discountCoupon == -1)
     {
-     Console.Write("quantity:");
-     var itemQuantity = int.Parse(Console.ReadLine());
-     if (currentItem.QuantityCoupon != 0) { itemQuantity -= Math.Abs(itemQuantity / currentItem.QuantityCoupon); }
-     itemPrice = itemQuantity * currentItem.Price;
-     itemTotalPrice += itemPrice;
+     try { discountCoupon = int.Parse(ReadLine()); }
+     catch (Exception) { WriteLine("Incorect value, try again"); }
     }
-    Console.WriteLine("{0}:{1}$", currentItem.Name.TrimEnd(), itemPrice);
-    allItems.Add(new Item(){Name = currentItem.Name.TrimEnd(),Price = itemPrice});
-
+    foreach (var item in allItems) { WriteLine("{0}:{1}$", item.Name.TrimEnd(), item.Price); }
+    WriteLine("Total Price:{0}$", itemTotalPrice - itemTotalPrice * discountCoupon * 0.01);
+    ReadLine();
    }
-   Console.WriteLine("Enter %  discount coupons of total price");
-   var discountCoupon = int.Parse(Console.ReadLine());
-
-
-   foreach (var item in allItems)
-   {
-    Console.WriteLine("{0}:{1}$", item.Name.TrimEnd(), item.Price);
-   }
-
-
-  Console.WriteLine("Total Price:{0}$",  itemTotalPrice - itemTotalPrice*discountCoupon*0.01);
-
-   Console.ReadLine();
   }
   private static Item GetItemId()
   {
@@ -61,12 +66,12 @@ namespace CashRegister
     try
     {
      CashRegisterDBContext dbContext = new CashRegisterDBContext();
-     var itemId = int.Parse(Console.ReadLine());
+     var itemId = int.Parse(ReadLine());
      if (itemId == 0) { return null; }
      item = dbContext.Items.Find(itemId);
-     if (item == null) { Console.WriteLine("item not found, try again"); }
+     if (item == null) { WriteLine("item not found, try again"); }
     }
-    catch (Exception e) { Console.WriteLine("Incorect item id, try again"); }
+    catch (Exception e) { WriteLine("Incorect item id, try again"); }
    }
    return item;
   }
